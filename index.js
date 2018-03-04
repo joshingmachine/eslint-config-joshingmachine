4d98c903d672da8adc1b981c0ee784b8a2fa5766
joshingmachine <josh@joshingmachine.com>
2018-03-04 00:07:02 -0500

    Starting to add rules :baby:

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 module.exports = {
-    extends: [],
+    extends: [
+       './rules/core/possible-errors.js',
+        './rules/core/best-practices.js',
+        './rules/core/strict-mode.js',
+        './rules/core/nodejs-and-commonjs.js',
+        './rules/core/stylistic-issues.js',
+        './rules/core/ecmascript-6.js',
+    ],
     rules: {},
 }
---
+++ rules/core/best-practices.js
@@ -0,0 +1,6 @@
+module.exports = {
+    rules: {
+        'accessor-pairs': 2,
+        'array-callback-return': 2,
+    },
+}
