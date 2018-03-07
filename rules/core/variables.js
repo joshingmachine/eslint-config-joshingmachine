3f5c24d52ae51cff0eda98ce6a913d8489a6fa5e
joshingmachine <josh@joshingmachine.com>
2018-03-07 07:58:04 -0500

    Added rules for variables

--- rules/core/variables.js
+++ rules/core/variables.js
@@ -1,5 +1,34 @@
 module.exports = {
     rules: {
-
+        'init-declarations': [ 'error', 'always' ],
+        'no-catch-shadow': 'error',
+        'no-delete-var': 'error',
+        'no-label-var': 'error',
+        'no-restricted-globals': [ 'error' ],
+        'no-shadow': [ 'error', {
+            'builtinGlobals': true,
+            'hoist': 'all',
+            'allow': [],
+        }],
+        'no-shadow-restricted-names': 'error',
+        'no-undef': [ 'error', {
+            'typeof': false,
+        }],
+        'no-undef-init': 'error',
+        'no-undefined': 'error',
+        'no-unused-vars': [ 'error', {
+            'vars': 'all',
+            'varsIgnorePattern': '',
+            'args': 'all',
+            'ignoreRestSiblings', false,
+            'argsIgnorePattern': '',
+            'caughtErrors': 'all',
+            'caughtErrorsIgnorePattern': '',
+        }],
+        'no-use-before-define': [ 'error', {
+            'functions': false,
+            'classes': true,
+            'variables': true,
+        }],
     },
 }
