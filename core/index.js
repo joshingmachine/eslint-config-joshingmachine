1de21f00f9a11072cd775eeb2b7a721260a10ae3
joshingmachine <josh@joshingmachine.com>
2018-03-07 08:09:20 -0500

    Updated rule directories for greater modularity

---
+++ core/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+    extends: [
+        './possible-errors.js',
+        './best-practices.js',
+        './strict-mode.js',
+        './nodejs-and-commonjs.js',
+        './stylistic-issues.js',
+        './ecmascript-6.js',
+    ],
+}
--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 module.exports = {
     extends: [
-       './rules/core/possible-errors.js',
-        './rules/core/best-practices.js',
-        './rules/core/strict-mode.js',
-        './rules/core/nodejs-and-commonjs.js',
-        './rules/core/stylistic-issues.js',
-        './rules/core/ecmascript-6.js',
+        './core',
     ],
     rules: {},
 }
