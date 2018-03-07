84d5b3f101c58aa5d29a99d7979df2ae3db950b4
joshingmachine <josh@joshingmachine.com>
2018-03-07 13:29:33 -0500

    Added possible error rules

--- core/possible-errors.js
+++ core/possible-errors.js
@@ -1,5 +1,33 @@
 module.exports = {
     rules: {
+        'for-direction': 'error',
+        'getter-return': [ 'error', {
+            'allowImplicit': false,
+        }],
+        'no-await-in-loop': 'error',
+        'no-compare-neg-zero': 'error',
+        'no-cond-assign': [ 'error', 'always' ],
+        'no-console': [ 'error', {
+            'allow': [],
+        }],
+        'no-constant-condition': [ 'error', {
+            'checkLoops': true,
+        }],
+        'no-control-regex': 'error',
+        'no-debugger': 'error',
+        'no-dupe-args': 'error',
+        'no-dupe-keys': 'error',
+        'no-duplicate-case': 'error',
+        'no-empty': [ 'error', {
+            'allowEmptyCatch': false,
+        }],
+        'no-empty-character-class': 'error',
+        'no-ex-assign': 'error',
+        'no-extra-boolean-cast': 'error',
+        'no-extra-parens': [ 'error', 'all', {
+            'conditionalAssign': true,
+            'returnAssign': true,
 
+        }],
     },
 }
