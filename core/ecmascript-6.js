13ff9512c0a65835147bb152facb9d1028e6fdef
joshingmachine <josh@joshingmachine.com>
2018-03-08 12:59:33 -0500

    Adding ES6 rules

--- core/ecmascript-6.js
+++ core/ecmascript-6.js
@@ -1,5 +1,31 @@
 module.exports = {
     rules: {
+        'arrow-body-style': [ 'error', 'as-needed', {
+            'requireReturnForObjectLiteral': false,
+        }],
+        'arrow-parens': [ 'error', 'as-needed', {
+            'requireForBlockBody': false,
+        }],
+        'arrow-spacing': [ 'error', {
+            'before': true,
+            'after': true,
+        }],
+        'constructor-super': 'error',
+        'generator-star-spacing': [ 'error', {
+            'before': true,
+            'after': false,
+            'anonymous': 'before',
+            'method': 'before',
+        }],
+        'no-class-assign': 'error',
+        'no-confusing-arrow': [ 'error', {
+            'allowParens': true,
+        }],
+        'no-const-assign': 'error',
+        'no-dupe-class-members': 'error',
+        'no-duplicate-imports': [ 'error', {
+            'includeExports': true,
+        }],
 
     },
 }
