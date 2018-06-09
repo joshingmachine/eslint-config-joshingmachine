4b318fa9cdb7878da0eeceee6bfaa6f23bd69d2c
joshingmachine <josh@joshingmachine.com>
2018-06-09 14:31:07 -0400

    Alphabetizing strings, good use of my time

--- core/index.js
+++ core/index.js
@@ -1,11 +1,11 @@
 module.exports = {
     extends: [
-        './possible-errors.js',
         './best-practices.js',
-        './strict-mode.js',
-        './variables.js',
-        './nodejs-and-commonjs.js',
-        './stylistic-issues.js',
         './ecmascript-6.js',
+        './nodejs-and-commonjs.js',
+        './possible-errors.js',
+        './strict-mode.js',
+        './stylistic-issues.js',
+        './variables.js',
     ],
 }
